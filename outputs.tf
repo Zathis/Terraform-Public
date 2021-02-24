2022d6d1705be04affa2eba0371c49e5afcbf40b
Ashley <53372051+crandon42@users.noreply.github.com>
2021-02-24 11:23:27 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,5 @@ output "network_security_group_name" {
 }
 
 output "network_security_group_id" {
-  value = azurerm_network_security_group.nsg.id
+ value = azurerm_subnet_network_security_group_association.nsg.id
 }
