64e4829c007cc5ddc9f920016292ef5ef2ceba6d
Ashley <53372051+crandon42@users.noreply.github.com>
2021-02-24 10:06:49 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
 output "network_security_group_name" {
   value = azurerm_network_security_group.nsg.name
 }
-
-output "network_security_group_id" {
-  value = azurerm_network_security_group.nsg.id
-}
-
-output "network_interface_id" {
-  value = azurerm_subnet_network_security_group_association.spokesubnet.id
-}
